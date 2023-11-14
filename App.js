1e9ed4c1e8c6db46cd79393cde46700638bcd6e3
Guiribe65 <112036526+Guiribe65@users.noreply.github.com>
2023-11-14 11:55:35 -0300

    Add files via upload

---
+++ App.js
@@ -0,0 +1,14 @@
+// src/App.js
+import React from 'react';
+import Header from './Header'; // Importa o componente Header corretamente
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <p>Boraaaaaaaaaaaaaaaaaa.</p>
+    </div>
+  );
+}
+
+export default App;
---
+++ Header.js
@@ -0,0 +1,14 @@
+// Header.js
+import React from 'react';
+import './Header.css';
+
+const Header = () => {
+  return (
+    <header className="header">
+      <div className="logo">Logo da Minha Empresa</div>
+      <div className="login">Faça Login</div>
+    </header>
+  );
+}
+
+export default Header;
